0d902d1411b5a971c51eb050a8c2c35167c0bbcd
khee8469 <bcw1992@naver.com>
2025-12-01 09:29:08 +0900

    10번 프로젝트 생성

---
+++ Source/SP_10/SP_10.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SP_10.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SP_10, "SP_10" );
